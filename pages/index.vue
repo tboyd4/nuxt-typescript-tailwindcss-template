1d08ef5baa035f89885a705b6b8e10975615a65c
Tyler Boyd <tboyd.evan@gmail.com>
2021-07-15 22:50:50 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1 class="title">nuxt-tailwind-template</h1>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style>
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>
